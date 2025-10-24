a66a4c7ac09cb22e6073768c8cb49f516d4c5d3a
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-10-24 11:36:21 +0200

    Initial commit

---
+++ src/main/java/giuseppetuccilli/esercitazione_24_10_2025/Esercitazione24102025Application.java
@@ -0,0 +1,13 @@
+package giuseppetuccilli.esercitazione_24_10_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercitazione24102025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercitazione24102025Application.class, args);
+	}
+
+}
---
+++ src/test/java/giuseppetuccilli/esercitazione_24_10_2025/Esercitazione24102025ApplicationTests.java
@@ -0,0 +1,13 @@
+package giuseppetuccilli.esercitazione_24_10_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercitazione24102025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
